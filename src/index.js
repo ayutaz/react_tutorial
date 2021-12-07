5eb04bc614db40e34b4dd6286f926ce2eef306a6
ayutaz <ka1357amnbpdr@gmail.com>
2021-12-07 17:21:11 +0900

    feat: write click on console when click square button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => {
+                console.log("click")
+            }}>
                 {this.props.value}
             </button>
         );
@@ -13,7 +16,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={i}/>;
     }
 
     render() {
@@ -47,7 +50,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -61,6 +64,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
